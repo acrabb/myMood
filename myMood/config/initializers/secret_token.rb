2ac46363cf3c0fb9c15ffc314903f114af41f895
Emily Andrews <emily@tesba.net>
2013-10-18 10:31:01 -0700

    empty rails app

---
+++ myMood/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMood::Application.config.secret_token = '84106fd5c78885ea069bc0c7ab43be39385622c27f71b612155800a51b15d4da4abdd9de5786bd17edb9832a4c87e2f3f97dcebfaf631953ea422da64b052976'
