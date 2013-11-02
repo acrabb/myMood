7f6d626e5906ffa1cb5037f75e9f216e05ac4897
Emily Andrews <emily@tesba.net>
2013-11-02 13:12:48 -0700

    making fields accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # This might be temporary. We need this so the cucumber tests
+  # don't error out. It lets us create things in DB.
+  attr_accessible :phone_number, :name, :password, :email
+	
+	# Sargun- I have no idea what's going on. Please document.
 	before_save { self.email = email.downcase }
   before_create :create_remember_token
 
