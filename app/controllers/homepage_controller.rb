e4ea338d173cd0b1e12fb052871da46f37897d67
Andre Crabb <crabb.andre@gmail.com>
2013-11-02 14:09:00 -0700

    Changed signup page to use a form

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,12 @@
 class HomepageController < ApplicationController
 
   def index
+    logger.debug("HomePage > Index loading")
   
   end
 
   def signup
+    logger.debug("HomePage > SignUp loading")
 
   end
 
